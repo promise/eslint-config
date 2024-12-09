23be96af5ecd798f8eb62b7603c926be0bdc1854
Glenn <glenn@not-tech.support>
2024-12-09 16:01:56 +0000

    fix: nevermind
    Signed-off-by: Glenn <glenn@not-tech.support>

--- configs/typescript/main.js
+++ configs/typescript/main.js
@@ -20,7 +20,7 @@ export default {
   "@typescript-eslint/explicit-function-return-type": ["off"],
   "@typescript-eslint/explicit-member-accessibility": ["off"],
   "@typescript-eslint/explicit-module-boundary-types": ["error"],
-  "@typescript-eslint/member-ordering": ["error"],
+  "@typescript-eslint/member-ordering": ["off"],
   "@typescript-eslint/method-signature-style": ["error", "method"],
   "@typescript-eslint/naming-convention": ["off"],
   "@typescript-eslint/no-array-delete": ["error"],
